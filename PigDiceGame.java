c84eb147c8a7ee6086598a97564711330ac5fefd
SASI PRANEETH <98507903+18211a0222@users.noreply.github.com>
2022-07-02 13:32:10 +0530

    Update PigDiceGame.java

--- PigDiceGame.java
+++ PigDiceGame.java
@@ -12,18 +12,18 @@ public class PigDiceGame {
 		while (totalscore < 20) { // run a loop until the total score is less than 20
 			System.out.println("\nEnter 'r' to roll again, 'h' to hold. ");
 			RollorHold = sc.next().charAt(0); //reading the input character from the user
-			if (RollorHold == 'r') { // if it is r we roll the dice
-				int diceroll = (int) Math.floor(Math.random() * (6) + 1); //  this gives a random number between 1 and 6 as a dice
+			if (RollorHold == 'r') { // this if else is for 'r' or 'h' input from user if it is r we roll the dice
+				int diceroll = (int) Math.floor(Math.random() * (6) + 1); //  this gives a random number between 1 and 6 like a dice
 				System.out.println("You rolled: " + diceroll);
-				if (diceroll == 1) { // if the dice roll is 1 we end the turn and make the turn score zero as turn ended
+				if (diceroll == 1) { // in this if else, if the dice roll is 1 we end the turn and make the turn score zero as turn ended
 					System.out.println("Turn over. No Score");
 					turnscore = totalscore; // as turn ended, the turn score is made to total score we have saved by holding 
 					turn += 1; // as the turn ended we go to next turn
 					System.out.println("\nTURN " + turn + ":");
 					System.out.println("\nWelcome to the game of Pig! ");
 				} else {
-					turnscore += diceroll; // we add the dice value until the user holds the turn score
-					if (turnscore > 20) { // if the turn score is greater than 20, the user can hold and finish the game 
+					turnscore += diceroll; // we add the dice value until the user holds the present turn score
+					if (turnscore >= 20) { //in this if else, if the turn score is greater than 20, the user can just hold and finish the game 
 						System.out.println("Your turn score is " + turnscore + " and your total score is " + turnscore);
 						System.out.println("You Win! You finished in " + turn + " turns!");
 						System.out.println("\nGame over!");
